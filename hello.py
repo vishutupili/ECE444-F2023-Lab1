29cda4dc3adf04c4188e055e20a06e1e04a13948
Vishu Tupili <vtupili@Vishus-MacBook.local>
2023-09-23 21:48:35 -0400

    activity 4: modified hello.py and templates to reproduce example 4-7

--- hello.py
+++ hello.py
@@ -1,16 +1,27 @@
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask import Flask, render_template, redirect, flash, session, url_for
 from datetime import datetime
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'abc123'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
-def index():
-	return render_template('index.html', curr_time=datetime.utcnow())
+class NameForm(FlaskForm):
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
 
-# activity 3
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html', name=name, curr_time=datetime.utcnow())
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	form = NameForm()
+	if form.validate_on_submit():
+		prev_name = session.get('name')
+		if prev_name is not None and prev_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	
+	return render_template('index.html', form=form, name=session.get('name'))
